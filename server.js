af6aa1ea0dd589c45460d78339b3ed55819e97ed
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-03-07 22:05:34 -0600

    Added appropriate app. requests for each TODO item in server.js

--- server.js
+++ server.js
@@ -17,29 +17,35 @@ const db = mysql.createConnection(
 );
 
 // TODO: view all depts
-
+app.get
 //TODO: view all roles
-
+app.get
 //TODO: view all employees
-
+app.get
 //TODO: add department
-
+app.post
 //TODO: add a role
-
+app.post
 //TODO: add an employee
-
+app.post
 //TODO: Update an employee role
-
+app.put
 //TODO: Update employee managers
-
+app.put
 //TODO: View employees by manager
-
+app.get
 //TODO: View employees by department
-
+app.get
 //TODO: delete departments
-
+app.delete
 //TODO: delete roles
-
+app.delete
 //TODO: delete employees
-
+app.delete
 //TODO: view total utilized budget of a dept (combined salaries of all employees in dept)
+//app.?
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
